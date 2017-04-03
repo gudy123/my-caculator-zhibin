bd8564c5bd4457f6f7dc06f365a4d795cad6d2e0
XuZhiBin <1242018755@qq.com>
2017-04-04 00:34:55 +0800

    将布局改了一下，写了几个Button函数
    1、布局由以前的单纯颜色改为在背景插入图片，并使按钮背景透明（其中”#000000“绝对不能改一点，空格也不许加），并通过backgroundTint和backgroundTintMode使图片色调改变，为方便看，字体改为白色。
    2、平方、平方根、倒数、CE、正负号、小数点现在都可以正常使用了（至少现在发现的bug都解决了）。

--- calculator/app/src/main/java/com/example/calculator/MainActivity.java
+++ calculator/app/src/main/java/com/example/calculator/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.calculator;
 import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -11,20 +12,21 @@ import android.view.View.OnClickListener;
 import java.io.StringBufferInputStream;
 
 import static android.R.attr.button;
+import static android.R.attr.flipInterval;
 
 public class MainActivity extends AppCompatActivity implements  View.OnClickListener {
-
-
+    boolean ifOncePoint=true;
+    boolean ifResult=false;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
 //        获取TextView Text1 ，将其置为0
-        TextView tV=(TextView) findViewById(R.id.Text1);
-        tV.setText("0");
+        TextView resulte=(TextView) findViewById(R.id.Text1);
+        resulte.setText("0");
 
-        //    //添加按钮监听函数
+        //    添加按钮监听函数
         Button btn1 = (Button) findViewById(R.id.Btn1);
         Button btn2 = (Button) findViewById(R.id.Btn2);
         Button btn3 = (Button) findViewById(R.id.Btn3);
@@ -34,7 +36,7 @@ public class MainActivity extends AppCompatActivity implements  View.OnClickList
         Button btn7 = (Button) findViewById(R.id.Btn7);
         Button btn8 = (Button) findViewById(R.id.Btn8);
         Button btn9 = (Button) findViewById(R.id.Btn9);
-        Button btn0 = (Button) findViewById(R.id.BtnZero);
+        Button btn0 = (Button) findViewById(R.id.Btn0);
 
         btn1.setOnClickListener(this );
         btn2.setOnClickListener(this );
@@ -48,47 +50,149 @@ public class MainActivity extends AppCompatActivity implements  View.OnClickList
         btn0.setOnClickListener(this );
     }
     @Override
+
+//    数字键 onclick
     public void onClick(View v) {
-        TextView tV = (TextView) findViewById(R.id.Text1);
-        if (tV.getText() == "0") tV.setText("");
+        TextView resulte= (TextView) findViewById(R.id.Text1);
+        if (resulte.getText().equals("0")||(ifResult==true))
+        {
+           // Log.e("here","ppp");///调试用的案例
+            resulte.setText("");
+            ifResult=false;
+        }
         switch (v.getId()) {
             case R.id.Btn1:
-                tV.setText(tV.getText()+"1");
+                resulte.setText(resulte.getText()+"1");
                 break;
             case R.id.Btn2:
-                tV.setText(tV.getText()+"2");
+                resulte.setText(resulte.getText()+"2");
                 break;
             case R.id.Btn3:
-                tV.setText(tV.getText()+"3");
+                resulte.setText(resulte.getText()+"3");
                 break;
             case R.id.Btn4:
-                tV.setText(tV.getText()+"4");
+                resulte.setText(resulte.getText()+"4");
                 break;
             case R.id.Btn5:
-              tV.setText(tV.getText()+"5");
+              resulte.setText(resulte.getText()+"5");
                 break;
             case R.id.Btn6:
-              tV.setText(tV.getText()+"6");
+              resulte.setText(resulte.getText()+"6");
                 break;
             case R.id.Btn7:
-              tV.setText(tV.getText()+"7");
+              resulte.setText(resulte.getText()+"7");
                 break;
             case R.id.Btn8:
-              tV.setText(tV.getText()+"8");
+              resulte.setText(resulte.getText()+"8");
                 break;
             case R.id.Btn9:
-              tV.setText(tV.getText()+"9");
+              resulte.setText(resulte.getText()+"9");
                 break;
-            case R.id.BtnZero:
-                tV.setText(tV.getText()+"0");
+            case R.id.Btn0:
+                resulte.setText(resulte.getText()+"0");
                 break;
         }
+
     }
 
-    public void ClickBtnDelet(View v){
-        TextView tV=(TextView) findViewById(R.id.Text1);
-        StringBuffer str = new StringBuffer(R.id.Text1);
-        str.deleteCharAt(str.length()-1);
-        tV.setText(str.toString());
+    //  求”平方根“的点击事件
+    public void ClickBtnSqRoot(View v){
+        TextView resulte = (TextView) findViewById(R.id.Text1);
+        String textStr=resulte.getText().toString();
+        double lastNum =Double.parseDouble(textStr);
+        double  newNum =Math.sqrt(lastNum);
+        String secondString=Double.toString(newNum);
+        int endline=secondString.length();
+
+        if(textStr.charAt(0)=='-')  {       //判断数字是否为负数
+            resulte.setText("wrong");
+            return;
+        }
+        if(endline>=8)  endline=8;
+        String newStr=Double.toString(newNum).substring(0,endline);       ///设置输出位数不大于8位字符
+
+        resulte.setText(newStr);
+        ifResult=true;
+    }
+
+//    “平方”的点击事件
+    public void ClickBtnSqua(View v){
+        TextView resulte = (TextView) findViewById(R.id.Text1);
+        String textString=resulte.getText().toString();
+        double lastNum = Double.parseDouble(textString);
+        double  newNum = Math.pow(lastNum,2);
+        String secondString = Double.toString(newNum);
+        int endline=secondString.length();
+
+        if(endline>=8)  endline=8;
+        String newStr=Double.toString(newNum).substring(0,endline);       ///设置输出位数不大于8位字符
+
+        resulte.setText(newStr);
+        ifResult=true;
+    }
+//        “倒数”的点击事件
+    public void ClickBtnRecip(View v){
+        TextView resulte = (TextView) findViewById(R.id.Text1);
+        String textString=resulte.getText().toString();
+        double lastNum = Double.parseDouble(textString);
+        double  newNum = 1/lastNum;
+        String secondString = Double.toString(newNum);
+        int endline=secondString.length();
+
+        if(endline>=8)  endline=8;
+        String newStr=Double.toString(newNum).substring(0,endline);       ///设置输出位数不大于8位字符
+
+        resulte.setText(newStr);
+        ifResult=true;
+
+    }
+//    “删除”的点击事件
+    public void ClickBtnDelete(View v){
+        TextView resulte=(TextView) findViewById(R.id.Text1);
+        String str=resulte.getText().toString();     ///getText!!!s
+        if(str.charAt(str.length()-1)=='.')   ifOncePoint=true;
+        if(str.length()==1) {
+            resulte.setText("0");
+            return ;
+        }
+        if(str.equals("0")) return ;
+        resulte.setText(str.substring(0,str.length()-1));
+//        StringBuffer str = new StringBuffer(resulte.getText().toString());
+//        str.deleteCharAt(str.length()-1);
+//        resulte.setText(str.toString());
+    }
+
+//    “CE” 的点击事件
+    public void ClickBtnCE (View v){
+        TextView resulte = (TextView) findViewById(R.id.Text1);
+        resulte.setText("0");
+        ifOncePoint=true;
+        ifResult=false;
+    }
+
+//    “正负转换” 的点击事件
+    public void ClickBtnPorN(View v){
+        TextView resulte = (TextView) findViewById(R.id.Text1);
+        String str=resulte.getText().toString();
+        if(str.charAt(0)=='-')
+            resulte.setText(str.substring(1,str.length()));
+        else if((str.charAt(0)=='0')&&(str.length()==1))
+            return;
+        else
+            resulte.setText("-"+str);
+
+    }
+
+//    “小数点”  的点击事件
+    public void ClickBtnPoint(View v){
+        TextView resulte = (TextView) findViewById(R.id.Text1);
+        String str=resulte.getText().toString();
+        if(ifOncePoint) {
+            resulte.setText(str+".");
+            ifOncePoint=false;       /// 等号 && delete && CE && C 中最后要变为true！！！！！！！！！！！！！！！！！！！！！！！！！！！
+        }
+        else {
+            return;
+        }
     }
 }
