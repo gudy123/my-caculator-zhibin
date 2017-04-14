8b2bfd0028bbe1e89dc0c9a04fae1d716b5b18b3
XuZhiBin <1242018755@qq.com>
2017-04-15 01:12:19 +0800

    在邓同学帮助下所有运算都已经实现
    基本完成

--- calculator/app/src/main/java/com/example/calculator/MainActivity.java
+++ calculator/app/src/main/java/com/example/calculator/MainActivity.java
@@ -17,24 +17,31 @@ import static android.R.attr.elegantTextHeight;
 import static android.R.attr.flipInterval;
 
 public class MainActivity extends AppCompatActivity  {
-    boolean ifOncePoint = true;
-    boolean ifResult = false;
-    double firstNum = 0;
-    boolean ifFirst = true;
-    int operateID = 0;
+    boolean ifOncePoint = true; //判断是否第一个小数点
+    boolean ifResult = false;   //是否结果
+    boolean ifFirst = true;     //是否是一个数的开始
+    boolean ifOne = true;       //是否第一次按等号
+    boolean ifOneNum = true;    //是否前面只有一个操作数
+    int operateID = 0;          //操作符ID
+    int operateID2 = 0;
+    double firstNum = 0;        //第一个数
+    double secondNum = 0;       //连等的最后一个最初输入数
+    double nowNum = 0;
     ///  “数字”  点击事件
     private View.OnClickListener NumListener1 = new View.OnClickListener(){
         @Override
         public void onClick(View view) {
             TextView resulte = (TextView) findViewById(R.id.Text1);
-            if(resulte.getText().toString().equals("TOO BIG"))   return;
-            if (resulte.getText().equals("0")||(ifResult==true) ||(operateID >= 1)){
-                resulte.setText("");
-                ifResult=false;
+            if(resulte.getText().toString().equals("TOO LONG"))   return;
+            if (ifFirst||ifResult){
+                if(resulte.getText().equals("0."));
+                else resulte.setText("");
+                ifResult = false;
+                ifFirst = false;
             }
             Button btn = (Button) view;
             resulte.setText(resulte.getText()+ btn.getText().toString());
-            ifFirst = true;
+            operateID2 = 1;
         }
     };
     private void setNumButtonListener(int viewId){     ///绑定数字键监听器
@@ -47,14 +54,26 @@ public class MainActivity extends AppCompatActivity  {
         public void onClick(View view) {
             TextView resulte = (TextView) findViewById(R.id.Text1);
             Button btn = (Button) view;
-            if(resulte.getText().toString().equals("TOO BIG"))   return;
+            if(resulte.getText().toString().equals("TOO LONG"))   return;
+            if(operateID2 == 1) ClickBtnEqua2(view);
             firstNum = Double.parseDouble(resulte.getText().toString());
-            if(btn.getText().equals("÷"))   operateID = 1;
-            else if(btn.getText().equals("×"))  operateID = 2;
-            else if(btn.getText().equals("-"))  operateID = 3;
-            else if(btn.getText().equals("+"))  operateID = 4;
+            if(btn.getText().equals("÷"))   {
+                operateID = 1;
+            }
+            else if(btn.getText().equals("×")) {
+                operateID = 2;
+            }
+            else if(btn.getText().equals("-")){
+                operateID = 3;
+            }
+            else if(btn.getText().equals("+")) {
+                operateID = 4;
+            }
             ifOncePoint = true;
             ifFirst = true;
+            ifOne = true;
+            ifOneNum = false;
+            operateID2 = 0;
         }
     };
     private void setOperate1ButtonListener(int viewId){     ///绑定加减乘除监听器
@@ -67,7 +86,7 @@ public class MainActivity extends AppCompatActivity  {
         public void onClick(View view) {
             TextView resulte = (TextView) findViewById(R.id.Text1);
             Button btnLine1 = (Button) view;
-            if(resulte.getText().toString().equals("TOO BIG"))   return;
+            if(resulte.getText().toString().equals("TOO LONG"))   return;
             String textStr = resulte.getText().toString();
             double lastNum = Double.parseDouble(textStr);
             double newNum;
@@ -78,12 +97,12 @@ public class MainActivity extends AppCompatActivity  {
                 }
                 newNum = Math.sqrt(lastNum);
             }
-            else if(btnLine1.getText().equals("x²"))    newNum = Math.pow(lastNum,2);   
+            else if(btnLine1.getText().equals("x²"))    newNum = Math.pow(lastNum,2);
             else newNum = 1 / lastNum;
 
             String newStr = Double.toString( newNum );
-            int endline = newStr.length();
-            if( endline > 16 )  resulte.setText( "TOO BIG" );
+            int strlength = newStr.length();
+            if( strlength > 16 && newNum > 1)  resulte.setText( "TOO LONG" );
             else if(newStr.substring(newStr.length()-2,newStr.length()).equals(".0")){
                 resulte.setText(newStr.substring(0,newStr.length()-2));
             }
@@ -124,7 +143,7 @@ public class MainActivity extends AppCompatActivity  {
     public void ClickBtnDelete(View v){                        //    “删除”的点击事件
         TextView resulte=(TextView) findViewById(R.id.Text1);
         ifFirst = true;
-        if(resulte.getText().toString().equals("TOO BIG"))   return;
+        if(resulte.getText().toString().equals("TOO LONG"))   return;
         String str=resulte.getText().toString();     ///getText!!!s
         if(ifResult==true) return; ///如果是结果就不能删
         if(str.charAt(str.length()-1)=='.')   ifOncePoint=true;
@@ -147,19 +166,20 @@ public class MainActivity extends AppCompatActivity  {
 //       “ C” 的点击事件
     public  void ClickBtnC(View v){
         TextView resulte = (TextView) findViewById(R.id.Text1);
-        if(resulte.getText().toString().equals("TOO BIG"))   return;
+        if(resulte.getText().toString().equals("TOO LONG"))   return;
         resulte.setText("0");
         firstNum=0;
         ifOncePoint=true;
         ifResult=false;
         ifFirst = true;
+        ifOne = true;
         operateID = 0;
     }
 //    “正负转换” 的点击事件
     public void ClickBtnPorN(View v){
         TextView resulte = (TextView) findViewById(R.id.Text1);
         String str=resulte.getText().toString();
-        if(str.equals("TOO BIG"))   return;
+        if(str.equals("TOO LONG"))   return;
         if(str.charAt(0)=='-')
             resulte.setText(str.substring(1,str.length()));
         else if((str.charAt(0)=='0')&&(str.length()==1))
@@ -169,28 +189,29 @@ public class MainActivity extends AppCompatActivity  {
     }
 //    “小数点”  的点击事件
     public void ClickBtnPoint(View v){
-        TextView resulte = (TextView) findViewById(R.id.Text1);
+        TextView resulte  = (TextView) findViewById(R.id.Text1);
         String str=resulte.getText().toString();
-        if(str.equals("TOO BIG"))   return;
-        if(ifFirst == false){
+        if(str.equals("TOO LONG"))   return;
+        if(ifResult == true||ifFirst == true ||resulte.getText().toString().equals("0")){
             resulte.setText("0.");
-            ifFirst = true;
-            return;
-        }else if(ifOncePoint) {
+            ifResult = false;
+            ifFirst = false;
+        }else if(resulte.getText().toString().contains(".")) return;
+        else if(!ifFirst){
             resulte.setText(str+".");
-            ifOncePoint=false;       /// 等号 && delete && CE && C 中最后要变为true！！！！！！！！！！！！！！！！！！！！！！！！！！！
-        }else {
-            return;
         }
+        ifOncePoint=false;       /// 等号 && delete && CE && C 中最后要变为true！！！！！！！！！！！！！！！！！！！！！！！！！！！
     }
 //     “等号”的点击事件
     public  void ClickBtnEqua(View v){
         TextView resulte = (TextView)findViewById(R.id.Text1);
-        double nowNum = Double.parseDouble(resulte.getText().toString());
+        nowNum = Double.parseDouble(resulte.getText().toString());
         String newStr;
+        if(ifOne) secondNum = nowNum;
         switch (operateID){
             case 1:
-                nowNum /= firstNum;
+                if(ifOne)   nowNum = firstNum / nowNum;
+                else nowNum = nowNum / secondNum;
                 newStr = Double.toString(nowNum);
                 if(newStr.substring(newStr.length()-2,newStr.length()).equals(".0")){
                     resulte.setText(newStr.substring(0,newStr.length()-2));
@@ -198,7 +219,8 @@ public class MainActivity extends AppCompatActivity  {
                 else resulte.setText(newStr);
                 break;
             case 2:
-                nowNum *= firstNum;
+                if(ifOne)   nowNum *= firstNum;
+                else nowNum *= secondNum;
                 newStr = Double.toString(nowNum);
                 if(newStr.substring(newStr.length()-2,newStr.length()).equals(".0")){
                     resulte.setText(newStr.substring(0,newStr.length()-2));
@@ -206,7 +228,8 @@ public class MainActivity extends AppCompatActivity  {
                 else resulte.setText(newStr);
                 break;
             case 3:
-                nowNum = firstNum-nowNum;
+                if(ifOne)   nowNum = firstNum-nowNum;
+                else nowNum = nowNum - secondNum;
                 newStr = Double.toString(nowNum);
                 if(newStr.substring(newStr.length()-2,newStr.length()).equals(".0")){
                     resulte.setText(newStr.substring(0,newStr.length()-2));
@@ -214,20 +237,62 @@ public class MainActivity extends AppCompatActivity  {
                 else resulte.setText(newStr);
                 break;
             case 4:
-                nowNum += firstNum;
+                if(ifOne)   nowNum += firstNum;
+                else nowNum +=secondNum;
                 newStr = Double.toString(nowNum);
                 if(newStr.substring(newStr.length()-2,newStr.length()).equals(".0")){
                     resulte.setText(newStr.substring(0,newStr.length()-2));
                 }
                 else resulte.setText(newStr);
                 break;
-            default:break;   ///连加连减处理
-        }
-        if(ifFirst){
-            firstNum = nowNum - firstNum;
-            ifFirst = false;
         }
         ifResult = true;
         ifOncePoint = true;
+        ifOne = false;
+        ifOneNum = true;
+    }
+    // 加减乘除运算里的等号
+    public  void ClickBtnEqua2(View v){
+        TextView resulte = (TextView)findViewById(R.id.Text1);
+        if(ifOneNum)    return ;
+        else {
+            secondNum = Double.parseDouble(resulte.getText().toString());
+            String newStr;
+            switch (operateID){
+                case 1:
+                    nowNum = firstNum / secondNum;
+                    newStr = Double.toString(nowNum);
+                    if(newStr.substring(newStr.length()-2,newStr.length()).equals(".0")){
+                        resulte.setText(newStr.substring(0,newStr.length()-2));
+                    }
+                    else resulte.setText(newStr);
+                    break;
+                case 2:
+                    nowNum = firstNum*secondNum;
+                    newStr = Double.toString(nowNum);
+                    if(newStr.substring(newStr.length()-2,newStr.length()).equals(".0")){
+                        resulte.setText(newStr.substring(0,newStr.length()-2));
+                    }
+                    else resulte.setText(newStr);
+                    break;
+                case 3:
+                    nowNum = firstNum - secondNum;
+                    newStr = Double.toString(nowNum);
+                    if(newStr.substring(newStr.length()-2,newStr.length()).equals(".0")){
+                        resulte.setText(newStr.substring(0,newStr.length()-2));
+                    }
+                    else resulte.setText(newStr);
+                    break;
+                case 4:
+                    nowNum = firstNum+secondNum;
+                    newStr = Double.toString(nowNum);
+                    if(newStr.substring(newStr.length()-2,newStr.length()).equals(".0")){
+                        resulte.setText(newStr.substring(0,newStr.length()-2));
+                    }
+                    else resulte.setText(newStr);
+                    break;
+            }
+            ifResult = true;
+        }
     }
 }
